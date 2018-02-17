0da173df06e165b25e9a261c9f5603fa3f8359be
John Ajiboye <ajiboye.o.john@gmail.com>
2018-02-18 01:25:22 +0200

    Updated Registration Controller

--- main.js
+++ main.js
@@ -1,35 +1,35 @@
-var coinApp = angular.module('coinApp', ['tw-currency-select','ngMaterialDatePicker']);
-coinApp.controller('mainController', function($scope, $http) {
-	$scope.dateTimeStart="2018-01-10";
-	$scope.dateTimeEnd="2018-01-18";
-	
-	var startDate =moment($scope.dateTimeStart).format('YYYY-MM-DD');
-	var endDate= moment($scope.dateTimeEnd).format('YYYY-MM-DD');
-console.log(endDate);
+var coinApp = angular.module('coinApp', ['tw-currency-select', 'ngMaterialDatePicker']);
+coinApp.controller('mainController', function ($scope, $http) {
+  $scope.dateTimeStart = "2018-01-10";
+  $scope.dateTimeEnd = "2018-01-18";
 
-    $scope.title = "Bitcoin Exchange";
-    $scope.currencyChangeCount = 0;
-    $scope.currencyCodeChangeCount = 0;
+  var startDate = moment($scope.dateTimeStart).format('YYYY-MM-DD');
+  var endDate = moment($scope.dateTimeEnd).format('YYYY-MM-DD');
+  console.log(endDate);
 
-    $scope.total_usd_amount = 0.0;
-    $scope.total_currency_amount = 0.0;
-    $scope.total_usd_units = 0.0;
-    $scope.total_currency_units = 0.0;
-   
-    $scope.currency_to_convert_to =  {code: 'NGN',  name: 'Nigerian Naira'};
+  $scope.title = "Bitcoin Exchange";
+  $scope.currencyChangeCount = 0;
+  $scope.currencyCodeChangeCount = 0;
 
-    $scope.currenciesWithNames = [
-      {code: 'EUR',  name: 'Euro'},
-      {code: 'GBP', name: 'British Pound'},
-      {code: 'NGN',  name: 'Nigerian Naira'},
-      {code: 'CNY',  name: 'Chinese Yuan'},
-      {code: 'CAD',  name: 'Canadian Dollars'},
-      {code: 'JPY',  name: 'Japanese yen'},
-      {code: 'CHF',  name: 'Swiss franc'},
-      {code: 'RUB',  name: 'Russian rouble'},
-      {code: 'ZAR',  name: 'South African Rand'},
-      {code: 'SGD',  name: 'Singapore dollar'}
-      
+  $scope.total_usd_amount = 0.0;
+  $scope.total_currency_amount = 0.0;
+  $scope.total_usd_units = 0.0;
+  $scope.total_currency_units = 0.0;
+
+  $scope.currency_to_convert_to = { code: 'NGN', name: 'Nigerian Naira' };
+
+  $scope.currenciesWithNames = [
+    { code: 'EUR', name: 'Euro' },
+    { code: 'GBP', name: 'British Pound' },
+    { code: 'NGN', name: 'Nigerian Naira' },
+    { code: 'CNY', name: 'Chinese Yuan' },
+    { code: 'CAD', name: 'Canadian Dollars' },
+    { code: 'JPY', name: 'Japanese yen' },
+    { code: 'CHF', name: 'Swiss franc' },
+    { code: 'RUB', name: 'Russian rouble' },
+    { code: 'ZAR', name: 'South African Rand' },
+    { code: 'SGD', name: 'Singapore dollar' }
+
   ];
 
   checkSession = function () {
@@ -38,77 +38,77 @@ console.log(endDate);
       window.location.href = "login.html";
     }
   }
-  
-  $scope.logout= function(){
+
+  $scope.logout = function () {
     window.sessionStorage.removeItem("login_token");
     alert(" You have Logged Out Succesfully")
     window.location.href = "login.html";
   }
-    $scope.getPrice = function() {
-      checkSession();
-      console.log("Trying to get price");   
-      $http({
-        method: 'GET',
-        url: 'https://api.coindesk.com/v1/bpi/currentprice/' + $scope.currency_to_convert_to.code + '.json'
-      }).then(function successCallback(response) {
-        $scope.rates = [];
-          var bpi = response.data.bpi;
-          var disclaimer = response.data.disclaimer;
-          $scope.disclaimer=disclaimer;
-          $scope.rates.push(bpi['USD']);
-          $scope.rates.push(bpi[$scope.currency_to_convert_to.code]);
+  $scope.getPrice = function () {
+    checkSession();
+    console.log("Trying to get price");
+    $http({
+      method: 'GET',
+      url: 'https://api.coindesk.com/v1/bpi/currentprice/' + $scope.currency_to_convert_to.code + '.json'
+    }).then(function successCallback(response) {
+      $scope.rates = [];
+      var bpi = response.data.bpi;
+      var disclaimer = response.data.disclaimer;
+      $scope.disclaimer = disclaimer;
+      $scope.rates.push(bpi['USD']);
+      $scope.rates.push(bpi[$scope.currency_to_convert_to.code]);
 
-          $scope.calculateTotal();
-          $scope.calculateUnits();
-        }, function errorCallback(response) {
-          console.error(response);
-        });
-    }
+      $scope.calculateTotal();
+      $scope.calculateUnits();
+    }, function errorCallback(response) {
+      console.error(response);
+    });
+  }
 
-    $scope.getData = function() {   
-     
-      $http({
-        method: 'GET',
-        url: "https://api.coindesk.com/v1/bpi/historical/close.json?start=" + startDate + "&end=" + endDate
-      }).then(function successCallback(response) {
-        
-       $scope.history=response.data.bpi;
-     
-        }, function errorCallback(response) {
-          console.error(response);
-        });
-    }
-      $scope.calculateTotal = function() {
-        console.log($scope.rates);
-        
-      angular.forEach($scope.rates, function(bt_rate) {
-       var total = bt_rate.rate_float * $scope.units;
-        
+  $scope.getData = function () {
+
+    $http({
+      method: 'GET',
+      url: "https://api.coindesk.com/v1/bpi/historical/close.json?start=" + startDate + "&end=" + endDate
+    }).then(function successCallback(response) {
+
+      $scope.history = response.data.bpi;
+
+    }, function errorCallback(response) {
+      console.error(response);
+    });
+  }
+  $scope.calculateTotal = function () {
+    console.log($scope.rates);
+
+    angular.forEach($scope.rates, function (bt_rate) {
+      var total = bt_rate.rate_float * $scope.units;
+
+      if (bt_rate.code == "USD") {
+        $scope.total_currency_amount = 0.00;
+        $scope.total_usd_amount = total;
+      } else {
+        $scope.total_currency_amount = total;
+        $scope.total_usd_amount = 0.00;
+      }
+    });
+  };
+
+  $scope.calculateUnits = function () {
+    angular.forEach($scope.rates, function (bt_rate) {
+      if (bt_rate.code == $scope.currency_to_convert_to.code) {
+        var units = parseInt($scope.amount) / bt_rate.rate_float;
         if (bt_rate.code == "USD") {
-          $scope.total_currency_amount = 0.00;
-          $scope.total_usd_amount = total;      
+          $scope.total_usd_units = units;
+          $scope.total_currency_units = 0.00;
         } else {
-            $scope.total_currency_amount = total;
-            $scope.total_usd_amount = 0.00; 
+          $scope.total_currency_units = units;
+          $scope.total_usd_units = 0.00;
         }
-      });
-    };
-   
-    $scope.calculateUnits = function() {
-      angular.forEach($scope.rates, function(bt_rate) {
-        if (bt_rate.code == $scope.currency_to_convert_to.code) {
-          var units = parseInt($scope.amount) / bt_rate.rate_float;
-          if (bt_rate.code == "USD") {
-            $scope.total_usd_units = units;
-            $scope.total_currency_units = 0.00;
-          } else {
-           $scope.total_currency_units = units;
-            $scope.total_usd_units = 0.00;
-          }
-        }
-      });
-    };
-  });
+      }
+    });
+  };
+});
 
 coinApp.controller('LoginController', function ($scope, $http) {
   $scope.init = function () {
@@ -119,10 +119,15 @@ coinApp.controller('LoginController', function ($scope, $http) {
   }
 
   $scope.handleFormSubmit = function () {
-       console.log("submitted: ", $scope.email, $scope.passwd);
+    //console.log("submitted: ", $scope.email, $scope.passwd);
+    if (typeof $scope.email == "undefined" || typeof $scope.passwd == "undefined") {
+      alert("Ensure you enter correct email and password");
+      return;
+    }
+
     $http({
       method: 'POST',
-      url: 'https://reqres.in/api/login',
+      url: 'http://localhost:5000/api/login',
       data: {
         "email": $scope.email,
         "password": $scope.passwd
@@ -136,7 +141,7 @@ coinApp.controller('LoginController', function ($scope, $http) {
         alert("Login failed unknown error");
       }
     }, function errorCallback(response) {
-      alert("Login failed: "+response.data.error);
+      alert("Login failed: " + response.data.message);
     });
   };
 });
@@ -153,23 +158,30 @@ coinApp.controller('RegisterController', function ($scope, $http) {
   }
 
   $scope.handleFormRegister = function () {
+
+    if (typeof $scope.email == "undefined" || typeof $scope.passwd == "undefined") {
+      alert("Ensure you enter correct email and password");
+      return;
+    }
+
     console.log("we are here")
-    console.log("submitted: ", $scope.username, $scope.passwd, $scope.username,
- $scope.name,
-    $scope.email,
-   $scope.currency,
-     $scope.passwd,
-    $scope.confirmpasswd);
+    console.log("submitted: ", 
+      $scope.first_name,
+      $scope.last_name,
+      $scope.email,
+      $scope.currency,
+      $scope.password,
+      $scope.confirmpassword);
     $http({
       method: 'POST',
-      url: 'https://reqres.in/api/register',
+      url: 'http://localhost:5000/api/register',
       data: {
-        "username": $scope.username,
-        "name": $scope.name,
+        "first_name": $scope.first_name,
+        "last_name": $scope.last_name,
         "email": $scope.email,
         "currency": $scope.currency,
-        "password": $scope.passwd,
-        "confirmpasswd": $scope.confirmpasswd
+        "password": $scope.password,
+        "confirmpasswd": $scope.confirmpassword
       }
     }).then(function successCallback(response) {
       if (!!response.data.token) {
@@ -180,7 +192,7 @@ coinApp.controller('RegisterController', function ($scope, $http) {
         alert("Login failed unknown error");
       }
     }, function errorCallback(response) {
-      alert("Login failed: "+response.data.error);
+      alert("Login failed: " + response.data.error);
     });
   };
 
